99d21e67578a7a47e71d5ca668f1b82d97dfb3e2
Oana-Petrescu <35079753+Oana-Petrescu@users.noreply.github.com>
2018-02-03 12:13:48 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,7 +6,7 @@
 const table = $('#pixelCanvas');
 var mouseDown = false;
 
-// creates the elements of the table - rows and cells
+// Creates the elements of the table - rows and cells and appends them to the initial <table> tag
 function makeGrid(rows,cols) {
 	for (var i = 0; i < rows; i++) {
 		var tablerow = table.append('<tr></tr>');
@@ -16,24 +16,24 @@ function makeGrid(rows,cols) {
 	}
 }
 
-// Display table/Erase current content when Create button is clicked
+// Display table / Erase current content when Create button is clicked
 $('#createBtn').on('click', function (e) {
-				e.preventDefault();
-				table.children().remove();
-				var inputRows = $('#rows').val();
-				var inputCols = $('#columns').val();
-				makeGrid(inputRows,inputCols);
+	e.preventDefault();
+	table.children().remove();
+	var inputRows = $('#rows').val();
+	var inputCols = $('#columns').val();
+	makeGrid(inputRows,inputCols);
 });
 
 // Reset table to original size when the Reset button is clicked
 $('#resetBtn').on('click', function (e) {
-				e.preventDefault();
-				table.children().remove();
-				var inputRows = $('#rows')[0].defaultValue;
-				var inputCols = $('#columns')[0].defaultValue;
-				$('#rows').val(inputRows);
-				$('#columns').val(inputCols);
-				makeGrid(inputRows, inputCols);
+	e.preventDefault();
+	table.children().remove();
+	var inputRows = $('#rows')[0].defaultValue;
+	var inputCols = $('#columns')[0].defaultValue;
+	$('#rows').val(inputRows);
+	$('#columns').val(inputCols);
+	makeGrid(inputRows, inputCols);
 });
 
 //Add color to cell with one click
@@ -48,7 +48,7 @@ table.on('dblclick','td', function () {
     $(this).css('background', 'white');
 });
 
-// Color multiple cells when the mouse is moved and click is hold (mousedown)
+// Color multiple cells when the mouse is moved and click is hold 
 table.on('mousedown','td', function() {
 	mouseDown = true;
 });
