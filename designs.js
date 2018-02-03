eaf16987456c4606ca47e6fbec628446f188a199
Oana-Petrescu <35079753+Oana-Petrescu@users.noreply.github.com>
2018-02-03 12:08:32 +0200

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,20 +1,22 @@
 // Select color input
 // Select size input
-
 // When size is submitted by the user, call makeGrid()
 
+//Global variable declarations
 const table = $('#pixelCanvas');
-	
+var mouseDown = false;
+
+// creates the elements of the table - rows and cells
 function makeGrid(rows,cols) {
 	for (var i = 0; i < rows; i++) {
 		var tablerow = table.append('<tr></tr>');
-		for (var j = 0; j < cols; j++) {
+		 for (var j = 0; j < cols; j++) {
 			tablerow.children().last().append('<td></td>');
 		}
 	}
 }
-// display table when Create button is clicked
 
+// Display table/Erase current content when Create button is clicked
 $('#createBtn').on('click', function (e) {
 				e.preventDefault();
 				table.children().remove();
@@ -23,7 +25,7 @@ $('#createBtn').on('click', function (e) {
 				makeGrid(inputRows,inputCols);
 });
 
-// Reset table to original size 
+// Reset table to original size when the Reset button is clicked
 $('#resetBtn').on('click', function (e) {
 				e.preventDefault();
 				table.children().remove();
@@ -34,17 +36,31 @@ $('#resetBtn').on('click', function (e) {
 				makeGrid(inputRows, inputCols);
 });
 
-//Add color to the cell
+//Add color to cell with one click
 table.on('click','td',function() {
 		var color = $('#colorPicker').val();
 		$(this).css('background', color)
 });
 
 
-//Remove color from cell
+//Remove color from cell with dblclick
 table.on('dblclick','td', function () {
     $(this).css('background', 'white');
 });
 
+// Color multiple cells when the mouse is moved and click is hold (mousedown)
+table.on('mousedown','td', function() {
+	mouseDown = true;
+});
 
-// Your code goes here!
+table.on('mouseup','td', function() {
+	mouseDown = false;
+});
+
+table.on('mousemove','td', function() {
+	if(mouseDown === true) {
+		var color = $('#colorPicker').val();
+	  $(this).css('background', color)
+	}
+
+});
